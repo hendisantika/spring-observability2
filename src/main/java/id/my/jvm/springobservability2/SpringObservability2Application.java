3b835b36de91c397739090334f493042fe724005
hendisantika <hendisantika@yahoo.co.id>
2026-07-21 06:25:52 +0700

    feat: modify main app🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/jvm/springobservability2/SpringObservability2Application.java
+++ src/main/java/id/my/jvm/springobservability2/SpringObservability2Application.java
@@ -1,7 +1,14 @@
 package id.my.jvm.springobservability2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringObservability2Application {
@@ -10,4 +17,26 @@ public class SpringObservability2Application {
         SpringApplication.run(SpringObservability2Application.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @RestController
+    class HelloController {
+
+        private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+        private final RestTemplate restTemplate;
+
+        HelloController(RestTemplate restTemplate) {
+            this.restTemplate = restTemplate;
+        }
+
+        @GetMapping("/hello")
+        public String hello() {
+            LOGGER.info("---------Hello method started---------");
+            ResponseEntity<String> responseEntity = this.restTemplate.postForEntity("https://httpbin.org/post", "Hello, Cloud!", String.class);
+            return responseEntity.getBody();
+        }
+    }
 }
