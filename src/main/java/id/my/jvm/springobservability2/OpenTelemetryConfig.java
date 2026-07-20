3e741cfd4a21e2942574796657d9dfcf2f7681f5
hendisantika <hendisantika@yahoo.co.id>
2026-07-21 06:27:06 +0700

    feat: add OpenTelemetryConfig🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/jvm/springobservability2/OpenTelemetryConfig.java
@@ -0,0 +1,60 @@
+package id.my.jvm.springobservability2;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
+import io.opentelemetry.proto.resource.v1.internal.Resource;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-observability2
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/07/26
+ * Time: 06.26
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class OpenTelemetryConfig {
+    @Bean
+    OpenTelemetry openTelemetry(SdkLoggerProvider sdkLoggerProvider, SdkTracerProvider sdkTracerProvider, ContextPropagators contextPropagators) {
+        OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
+                .setLoggerProvider(sdkLoggerProvider)
+                .setTracerProvider(sdkTracerProvider)
+                .setPropagators(contextPropagators)
+                .build();
+        OpenTelemetryAppender.install(openTelemetrySdk);
+        return openTelemetrySdk;
+    }
+
+    @Bean
+    SdkLoggerProvider otelSdkLoggerProvider(Environment environment, ObjectProvider<LogRecordProcessor> logRecordProcessors) {
+        String applicationName = environment.getProperty("spring.application.name", "application");
+        Resource springResource = Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, applicationName));
+        SdkLoggerProviderBuilder builder = SdkLoggerProvider.builder()
+                .setResource(Resource.getDefault().merge(springResource));
+        logRecordProcessors.orderedStream().forEach(builder::addLogRecordProcessor);
+        return builder.build();
+    }
+
+    @Bean
+    LogRecordProcessor otelLogRecordProcessor() {
+        return BatchLogRecordProcessor
+                .builder(
+                        OtlpGrpcLogRecordExporter.builder()
+                                .setEndpoint("http://localhost:4317")
+                                .build())
+                .build();
+    }
+}
