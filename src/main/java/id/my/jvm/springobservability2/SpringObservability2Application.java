157c7f8c5070c6756ed5d361710781d96fb6a20b
hendisantika <hendisantika@yahoo.co.id>
2026-07-21 06:22:52 +0700

    docs: init project🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/jvm/springobservability2/SpringObservability2Application.java
@@ -0,0 +1,13 @@
+package id.my.jvm.springobservability2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringObservability2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringObservability2Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/jvm/springobservability2/SpringObservability2ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.jvm.springobservability2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringObservability2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
